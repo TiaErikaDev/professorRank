3c5c87a4b9017d867b901df5d65b1b60f529ae81
erikapimentelolx <erikapimentelolx@gmail.com>
2025-01-31 23:37:03 -0300

    ajuste para iterar sobre SEFOR

--- app/professorRank.py
+++ app/professorRank.py
@@ -43,79 +43,86 @@ def iniciar_consulta():
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    try:
-        driver.get("https://selecaoprofessor.seduc.ce.gov.br/login")
-        time.sleep(1)
+    driver.get("https://selecaoprofessor.seduc.ce.gov.br/login")
+    time.sleep(1)
 
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "cpf")))
-        driver.find_element(By.ID, "cpf").send_keys(cpf)
-        driver.find_element(By.ID, "pass").send_keys(senha)
-        driver.find_element(By.ID, "btLogin").click()
-        time.sleep(1)
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "cpf")))
+    driver.find_element(By.ID, "cpf").send_keys(cpf)
+    driver.find_element(By.ID, "pass").send_keys(senha)
+    driver.find_element(By.ID, "btLogin").click()
+    time.sleep(1)
 
-        URL_CREDE = "https://selecaoprofessor.seduc.ce.gov.br/chamada_selecao?crede=23&munic=1347"
-        driver.get(URL_CREDE)
-        time.sleep(1)
+    for i in range(21, 24):
+        try:
+            URL_CREDE = f"https://selecaoprofessor.seduc.ce.gov.br/chamada_selecao?crede={i}&munic=1347"
+            driver.get(URL_CREDE)
+            time.sleep(1)
 
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "table.table tbody tr")))
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "table.table tbody tr")))
 
-        lista_escolas = driver.find_elements(By.XPATH, "//a[contains(@onclick, 'prepare_exibir_inscritos_carencia')]")
+            lista_escolas = driver.find_elements(By.XPATH, "//a[contains(@onclick, 'prepare_exibir_inscritos_carencia')]")
+            
+            crede_name = 'SEFOR 1' if i == 21 else 'SEFOR 2' if i == 22 else 'SEFOR 3'
         
-        for escola_index in range(len(lista_escolas)):
-            count = 0  # Reinicia o contador para cada escola
-            try:
-                driver.get(URL_CREDE)
-                time.sleep(2)
-                escolas = WebDriverWait(driver, 5).until(
-                    EC.presence_of_all_elements_located((By.XPATH, "//a[contains(@onclick, 'prepare_exibir_inscritos_carencia')]"))
-                )
-                
-                nome_escola = driver.find_element(By.XPATH, f'//*[@id="container-body"]/div[1]/div/div/table/tbody/tr[{escola_index + 1}]/td[2]').text.split()
-                nome_formatado = " ".join(nome_escola[nome_escola.index('-') + 1:])
-                
-                resultado_text.insert(tk.END, f"Processando escola {escola_index + 1}: {nome_formatado}\n")    
-                
-                escolas[escola_index].click()  
-                time.sleep(1)  
-                
-                tabela = WebDriverWait(driver, 5).until(
-                    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="modal-inscritos"]/div/div/div[2]/table/tbody/tr'))
-                )
-
-                for linha in tabela:
-                    try:
-                        colunas = linha.find_elements(By.TAG_NAME, 'td')  
-                        
-                        if len(colunas) >= 5:
-                            posicao_candidato = colunas[4].text.strip()
-                            
-                            if posicao_candidato.isdigit() and int(posicao_candidato) < int(classificacao):
-                                count += 1  # Incrementa apenas se a posição do candidato for menor que a sua
-
-                    except Exception as e:
-                        resultado_text.insert(tk.END, f"Erro ao processar linha: {e}\n")
-
-                minha_colocacao = count + 1  # Sua classificação é o número de candidatos à frente + 1
-
-                resultado_text.insert(tk.END, f'Total de candidatos: {len(tabela)}\n')
-                if minha_colocacao <= 3:
-                    resultado_text.insert(tk.END, f"Sua classificação na escola {escola_index + 1}: {minha_colocacao}º lugar ✅ ✅ ✅\n")
-                else:
-                    resultado_text.insert(tk.END, f"Sua classificação na escola {escola_index + 1}: {minha_colocacao}º lugar\n")
-                resultado_text.insert(tk.END, "\n--------------------------------------------------------------\n\n")
-                
-                # Fechar o modal após processar todas as linhas
-                fechar_botao = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Fechar")]')))
-                fechar_botao.click()
-                time.sleep(1)
-        
-            except Exception as e:
-                resultado_text.insert(tk.END, f"Erro ao processar escola {escola_index + 1}: {e}\n")
-                driver.get(URL_CREDE)
-                time.sleep(1)
+            resultado_text.insert(tk.END, "\n")
+            resultado_text.insert(tk.END, f"--------------------------------{crede_name}--------------------------------\n")
+            resultado_text.insert(tk.END, f"Total de escolas na {crede_name}: {len(lista_escolas)}\n")
+            
+            for escola_index in range(len(lista_escolas)):
+                try:
+                    driver.get(URL_CREDE)
+                    time.sleep(2)
+                    escolas = WebDriverWait(driver, 5).until(
+                        EC.presence_of_all_elements_located((By.XPATH, "//a[contains(@onclick, 'prepare_exibir_inscritos_carencia')]"))
+                    )
                     
-    except Exception as e:
-        messagebox.showerror("Erro", f"Ocorreu um erro geral: {e}")
+                    nome_escola = driver.find_element(By.XPATH, f'//*[@id="container-body"]/div[1]/div/div/table/tbody/tr[{escola_index + 1}]/td[2]').text.split()
+                    nome_formatado = " ".join(nome_escola[nome_escola.index('-') + 1:])
+                    
+                    resultado_text.insert(tk.END, f"Processando escola {escola_index + 1}: {nome_formatado}\n")    
+                    
+                    escolas[escola_index].click()  
+                    time.sleep(1)  
+                    
+                    tabela = WebDriverWait(driver, 5).until(
+                        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="modal-inscritos"]/div/div/div[2]/table/tbody/tr'))
+                    )
+
+                    count = 0  # Reinicia o contador para cada escola
+                    for linha in tabela:
+                        try:
+                            colunas = linha.find_elements(By.TAG_NAME, 'td')  
+                            
+                            if len(colunas) >= 5:
+                                posicao_candidato = colunas[4].text.strip()
+                                
+                                if posicao_candidato.isdigit() and int(posicao_candidato) < int(classificacao):
+                                    count += 1  # Incrementa apenas se a posição do candidato for menor que a sua
+
+                        except Exception as e:
+                            resultado_text.insert(tk.END, f"Erro ao processar linha: {e}\n")
+
+                    # Fechar o modal após processar todas as linhas
+                    fechar_botao = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Fechar")]')))
+                    fechar_botao.click()
+                    time.sleep(1)
+                    
+                    resultado_text.insert(tk.END, f'Total de candidatos: {len(tabela)}\n')
+
+                    minha_colocacao = count + 1  # Sua classificação é o número de candidatos à frente + 1
+                    if minha_colocacao <= 3:
+                        resultado_text.insert(tk.END, f"Sua classificação na escola {escola_index + 1}: {minha_colocacao}º lugar ✅ ✅ ✅\n")
+                    else:
+                        resultado_text.insert(tk.END, f"Sua classificação na escola {escola_index + 1}: {minha_colocacao}º lugar\n")
+                    resultado_text.insert(tk.END, "\n-----------------------------------------------------------------------\n\n")
+                    
+                except Exception as e:
+                    resultado_text.insert(tk.END, f"Erro ao processar escola {escola_index + 1}: {e}\n")
+                    driver.get(URL_CREDE)
+                    time.sleep(1)
+                        
+        except Exception as e:
+            messagebox.showerror("Erro", f"Ocorreu um erro geral: {e}")
 
     driver.quit()
     resultado_text.insert(tk.END, "Consulta concluída!\n")
